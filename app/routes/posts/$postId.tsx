abed811f61e898be3b5bd085cb3a7dcca4d0bfaa
sunadoi <y.sunadoi@gmail.com>
2022-04-17 18:51:45 +0900

    feature: posts

---
+++ app/routes/posts/$postId.tsx
@@ -0,0 +1,44 @@
+import { json } from "@remix-run/node"
+import type { HeadersFunction, LoaderFunction } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+
+import type { MicroCMSContent } from "@/types/microcms"
+import { client } from "lib/client.server"
+
+export const headers: HeadersFunction = ({ loaderHeaders }) => {
+  const cacheControl = loaderHeaders.get("Cache-Control") ?? "max-age=0, s-maxage=60, stale-while-revalidate=60"
+  return {
+    "cache-control": cacheControl,
+  }
+}
+
+export const loader: LoaderFunction = async ({ params, request }) => {
+  const url = new URL(request.url)
+  const draftKey = url.searchParams.get("draftKey")
+  const content = await client
+    .get<MicroCMSContent>({
+      endpoint: "posts",
+      contentId: params.postId,
+      queries: { draftKey: draftKey ?? "" },
+    })
+    .catch(() => {
+      throw new Response("Content Not Found.", {
+        status: 404,
+      })
+    })
+
+  // 下書きの場合キャッシュヘッダを変更
+  const headers = draftKey ? { "Cache-Control": "no-store, max-age=0" } : undefined
+
+  return json(content, { headers })
+}
+
+export default function PostsId() {
+  const content = useLoaderData<MicroCMSContent>()
+
+  return (
+    <div>
+      <h1>{content.title}</h1>
+    </div>
+  )
+}
