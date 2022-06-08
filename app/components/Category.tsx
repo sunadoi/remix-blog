a7ac1ff9837b407bf8085954506036c1f3b1bb41
sunadoi <y.sunadoi@gmail.com>
2022-06-08 16:44:59 +0900

    fix: category sort

--- app/components/Category.tsx
+++ app/components/Category.tsx
@@ -22,25 +22,27 @@ export const Category: FC<CategoryProps> = ({ categories }) => {
         <Title order={4}>カテゴリー</Title>
       </Group>
       <Divider my="sm" size="sm" />
-      {Object.entries(categories).map(([category, count], index) => {
-        if (index > 10) return <></>
-        return (
-          <Group
-            key={category}
-            spacing="xs"
-            mb="sm"
-            className="cursor-pointer hover:opacity-80"
-            onClick={() => navigate(`/categories/${category}`)}
-          >
-            {isCategory(category) && (
-              <Image fit="contain" src={CategoryIconMap.get(category) ?? ""} width={32} height={32} />
-            )}
-            <Text sx={(theme) => ({ color: theme.other.secondary })}>
-              {category} ({count})
-            </Text>
-          </Group>
-        )
-      })}
+      {Object.entries(categories)
+        .sort((a, b) => b[1] - a[1])
+        .map(([category, count], index) => {
+          if (index > 10) return <></>
+          return (
+            <Group
+              key={category}
+              spacing="xs"
+              mb="sm"
+              className="cursor-pointer hover:opacity-80"
+              onClick={() => navigate(`/categories/${category}`)}
+            >
+              {isCategory(category) && (
+                <Image fit="contain" src={CategoryIconMap.get(category) ?? ""} width={32} height={32} />
+              )}
+              <Text sx={(theme) => ({ color: theme.other.secondary })}>
+                {category} ({count})
+              </Text>
+            </Group>
+          )
+        })}
       {Object.keys(categories).length >= 10 && (
         <Text color="blue" className="cursor-pointer hover:opacity-80" onClick={() => navigate("/categories")}>
           もっと見る
