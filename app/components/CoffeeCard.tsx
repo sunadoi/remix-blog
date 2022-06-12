8148bae96b5c31d5113ccbfce881411470f0ec8f
sunadoi <y.sunadoi@gmail.com>
2022-06-12 15:12:36 +0900

    fix: Coffee shadow

--- app/components/CoffeeCard.tsx
+++ app/components/CoffeeCard.tsx
@@ -9,7 +9,12 @@ export const CoffeeCard: FC = () => {
     <Card radius="md" p="sm" shadow="xs">
       <Group position="center" direction="column" spacing="sm" py="md">
         <Image src={CoffeePCImage} alt="" width={400} height={100} fit="contain" />
-        <Anchor href="https://www.buymeacoffee.com/ysuna" target="_blank" rel="noopener noreferrer">
+        <Anchor
+          href="https://www.buymeacoffee.com/ysuna"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="rounded-xl shadow-lg"
+        >
           <Image
             src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
             alt="Buy Me A Coffee"
@@ -26,7 +31,12 @@ export const CoffeeArea: FC = () => {
   return (
     <Group position="center" direction="column" spacing="sm" py="md">
       <Image src={CoffeeSPImage} alt="" width={200} height={200} fit="contain" />
-      <Anchor href="https://www.buymeacoffee.com/ysuna" target="_blank" rel="noopener noreferrer">
+      <Anchor
+        href="https://www.buymeacoffee.com/ysuna"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="rounded-xl shadow-lg"
+      >
         <Image
           src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
           alt="Buy Me A Coffee"
